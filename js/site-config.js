306ed5ed6c38d7bc5752bcd030e1b48a838db1c8
Vince Remedia Works <vinceremediaworks@gmail.com>
2026-09-22 16:13:20 -0700

    Refresh Beware Hood pink model to chic layered street vibe (per user).

--- js/site-config.js
+++ js/site-config.js
@@ -7,7 +7,7 @@
  */
 (function () {
   // Bump when lookbook JPGs change so browsers skip stale CDN/disk cache.
-  const ASSET_V = '20260923d';
+  const ASSET_V = '20260923e';
 
   const COLORWAYS = [
     { name: 'Pink', slug: 'pink', hex: '#e5b3c5' },
