d52de6148c859fd39f40e762b509c57a22474b12
Andrew Martinez <144518904+jimmythegod100@users.noreply.github.com>
2026-09-23 17:35:51 -0700

    Fix Mark dog logo placement on primary cards (urgent).

--- js/site-config.js
+++ js/site-config.js
@@ -7,7 +7,7 @@
  */
 (function () {
   // Bump when lookbook JPGs change so browsers skip stale CDN/disk cache.
-  const ASSET_V = '20260923m';
+  const ASSET_V = '20260923n';
 
   const COLORWAYS = [
     { name: 'Pink', slug: 'pink', hex: '#e5b3c5' },
